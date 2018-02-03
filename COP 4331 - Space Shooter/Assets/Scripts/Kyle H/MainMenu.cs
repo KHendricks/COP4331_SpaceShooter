901f8b3a7f9f17b8c124bb93929a3bc615c1126d
KHendricks <Khendricks0815@gmail.com>
2018-02-02 19:55:23 -0500

    Implementing google play services
    wip

--- COP 4331 - Space Shooter/Assets/Scripts/Kyle H/MainMenu.cs
+++ COP 4331 - Space Shooter/Assets/Scripts/Kyle H/MainMenu.cs
@@ -1,10 +1,46 @@
-﻿using System.Collections;
+﻿using GooglePlayGames;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour 
 {
+	private void Awake()
+	{
+		// Google Play Service
+		PlayGamesPlatform.Activate();
+		OnConnectionResponse(PlayGamesPlatform.Instance.localUser.authenticated);
+
+	}
+
+	private void OnConnectionResponse(bool status)
+	{
+		if (status)
+		{
+			GameObject disconnectedIcons = GameObject.Find("Disconnected");
+			GameObject connectedIcons = GameObject.Find("Connected");
+			disconnectedIcons.SetActive(false);
+			connectedIcons.SetActive(true);
+		}
+		else
+		{
+			GameObject connectedIcons = GameObject.Find("Connected");
+			GameObject disconnectedIcons = GameObject.Find("Disconnected");
+			disconnectedIcons.SetActive(true);
+			connectedIcons.SetActive(false);
+		}
+	}
+
+	// On button click ask for login
+	public void OnConnectClick()
+	{
+		Social.localUser.Authenticate((bool status) =>
+		{
+			OnConnectionResponse(status);
+		});
+	}
+
 	public void PlayGame()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
