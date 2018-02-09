58c62c3afacf35f056ea85ddbce1b192f9192e31
KHendricks <Khendricks0815@gmail.com>
2018-02-09 09:24:39 -0500

    Implemented simple pause screen
    TODO: Needs to stop the animations and forces as well.

--- COP 4331 - Space Shooter/Assets/Scripts/PauseMenu.cs
+++ COP 4331 - Space Shooter/Assets/Scripts/PauseMenu.cs
@@ -7,32 +7,17 @@ public class PauseMenu : MonoBehaviour
 	public static bool isPaused = false;
  	public GameObject pauseUI;
 
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Escape))
-		{
-			if (isPaused)
-			{
-				ResumeGame();
-			}
-			else
-			{
-				PauseGame();
-			}
-		}
-	}
-
 	public void ResumeGame()
 	{
 		isPaused = false;
 		pauseUI.SetActive(false);
-		Time.timeScale = 1f;
+		Time.timeScale = 1;
 	}
 
 	public void PauseGame()
 	{
 		isPaused = true;
 		pauseUI.SetActive(true);
-		Time.timeScale = 0f;
+		Time.timeScale = 0;
 	}
 }
