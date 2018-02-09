7de3de528edc330bb2264e921278dc31c6f7c287
Kyle Capehart <kyleacapehart@gmail.com>
2018-02-08 21:26:50 -0500

    Simple additions
    Created GameController, a win condition, and an update scene

---
+++ COP 4331 - Space Shooter/Assets/Portal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.name == "Ship")
+		{
+			SceneManager.LoadScene("Game");
+		}
+	}
+
+}
---
+++ COP 4331 - Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController game;
+	private GameObject winText;
+	public int enemyNumber = 0;
+
+	void Awake()
+	{
+		if (game == null)
+		{
+			game = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+
+		winText = GameObject.Find("Mission Success");
+	}
+
+	void Start () 
+	{
+		enemyNumber = (GameObject.FindGameObjectsWithTag("Enemy")).Length;
+	}
+	
+	void Update () 
+	{
+		if (enemyNumber <= 0)
+		{
+			winCondition();
+		}	
+	}
+
+	void winCondition()
+	{
+		winText.gameObject.SetActive(true);
+		new WaitForSeconds(10);
+		winText.gameObject.SetActive(false);
+
+		SceneManager.LoadScene("Upgrade");
+
+		return;
+	}
+}
