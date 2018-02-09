750a79a9e1738103a7ff1cea3abada27d876f0da
KHendricks <Khendricks0815@gmail.com>
2018-02-09 18:35:28 -0500

    Added simple wasd movement, camerafollow script

---
+++ COP 4331 - Space Shooter 3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody rb;
+
+	// Update is called once per frame
+	void FixedUpdate ()
+ 	{
+		if (Input.GetKey("w"))
+		{
+			rb.AddForce(0, 0, 1000 * Time.deltaTime);
+		}
+
+		if (Input.GetKey("s"))
+		{
+			rb.AddForce(0, 0, -1000 * Time.deltaTime);
+		}
+
+		if (Input.GetKey("d"))
+		{
+			rb.AddForce(1000 * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey("a"))
+		{
+			rb.AddForce(-1000 * Time.deltaTime, 0, 0);
+		}
+	}
+}
