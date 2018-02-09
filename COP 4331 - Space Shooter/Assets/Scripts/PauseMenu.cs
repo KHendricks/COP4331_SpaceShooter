488717edb2f235f9b44a70345c03f94d4eb87e09
KHendricks <Khendricks0815@gmail.com>
2018-02-09 09:28:10 -0500

    Revert "Implemented simple pause screen"
    This reverts commit 58c62c3afacf35f056ea85ddbce1b192f9192e31.

--- COP 4331 - Space Shooter/Assets/Scripts/PauseMenu.cs
+++ COP 4331 - Space Shooter/Assets/Scripts/PauseMenu.cs
@@ -7,17 +7,32 @@ public class PauseMenu : MonoBehaviour
 	public static bool isPaused = false;
  	public GameObject pauseUI;
 
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (isPaused)
+			{
+				ResumeGame();
+			}
+			else
+			{
+				PauseGame();
+			}
+		}
+	}
+
 	public void ResumeGame()
 	{
 		isPaused = false;
 		pauseUI.SetActive(false);
-		Time.timeScale = 1;
+		Time.timeScale = 1f;
 	}
 
 	public void PauseGame()
 	{
 		isPaused = true;
 		pauseUI.SetActive(true);
-		Time.timeScale = 0;
+		Time.timeScale = 0f;
 	}
 }
