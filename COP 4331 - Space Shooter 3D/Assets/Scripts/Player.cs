a6fa19a3c9eb8b3511fb2e6b007a3d03a251a98b
KHendricks <Khendricks0815@gmail.com>
2018-02-09 20:05:58 -0500

    Fixed issues

--- COP 4331 - Space Shooter 3D/Assets/Scripts/Player.cs
+++ COP 4331 - Space Shooter 3D/Assets/Scripts/Player.cs
@@ -4,11 +4,4 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	void OnCollisionEnemy(Collision other)
-	{
-		if (other.gameObject.GetComponent<StandardEnemy>())
-		{
-			Debug.Log("BOOM");
-		}
-	}
 }
