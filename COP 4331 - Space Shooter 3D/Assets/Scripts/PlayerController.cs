595edcf5ba85c17a8a7dfd9541e7996fd876c9ce
GBenitez6199 <36120411+GBenitez6199@users.noreply.github.com>
2018-02-09 20:32:59 -0500

    Basic rotating when moving sideways. Tweaked force values for movement.
    Rotates with sideways motion, but infinitely. Need to add a limit at some point.

--- COP 4331 - Space Shooter 3D/Assets/Scripts/PlayerController.cs
+++ COP 4331 - Space Shooter 3D/Assets/Scripts/PlayerController.cs
@@ -11,22 +11,27 @@ public class PlayerController : MonoBehaviour
  	{
 		if (Input.GetKey("w"))
 		{
-			rb.AddForce(0, 0, 1000 * Time.deltaTime);
+			rb.AddForce(0, 0, 5000 * Time.deltaTime);
 		}
 
 		if (Input.GetKey("s"))
 		{
-			rb.AddForce(0, 0, -1000 * Time.deltaTime);
+			rb.AddForce(0, 0, -4000 * Time.deltaTime);
 		}
 
 		if (Input.GetKey("d"))
 		{
-			rb.AddForce(1000 * Time.deltaTime, 0, 0);
+			rb.AddForce(3500 * Time.deltaTime, 0, 0);
+			rb.AddTorque (0, 0, -75 * Time.deltaTime);
 		}
 
 		if (Input.GetKey("a"))
 		{
-			rb.AddForce(-1000 * Time.deltaTime, 0, 0);
+			rb.AddForce(-3500 * Time.deltaTime, 0, 0);
+			rb.AddTorque (0, 0, 75 * Time.deltaTime);
+
+
+
 		}
 	}
 }
