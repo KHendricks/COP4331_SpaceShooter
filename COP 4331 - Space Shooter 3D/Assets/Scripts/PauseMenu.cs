a66061c5017cd25ae8e31fb6ac22a236b7dc2dd7
KHendricks <Khendricks0815@gmail.com>
2018-03-13 20:51:27 -0400

    Created a pause menu

---
+++ COP 4331 - Space Shooter 3D/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour 
+{
+	public GameObject pauseCanvas, notPausedCanvas;
+
+	public void Start()
+	{
+		notPausedCanvas.SetActive(true);
+		pauseCanvas.SetActive(false);
+	}
+
+	public void PauseGame()
+	{
+		Time.timeScale = 0f;
+		pauseCanvas.SetActive(true);
+		notPausedCanvas.SetActive(false);
+
+	}
+
+	public void ResumeGame()
+	{
+		Time.timeScale = 1f;
+		pauseCanvas.SetActive(false);
+		notPausedCanvas.SetActive(true);
+	}
+}
