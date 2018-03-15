5488dda1b22c98058c8ab7338856dcbf500c119b
KHendricks <Khendricks0815@gmail.com>
2018-03-15 00:41:45 -0400

    Added healthbar to the player and adjusted score ui

---
+++ COP 4331 - Space Shooter 3D/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour 
+{
+	public Slider slider;
+	public Text healthAmt;
+	private int maxHealth;
+	private int health;
+
+	private void Start()
+	{
+		maxHealth = GameController.instance.GetHealth();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		health = GameController.instance.GetHealth();
+		slider.value = Mathf.Clamp01((float)(health) / (float)(maxHealth));
+		healthAmt.text = health + " / " + maxHealth;
+	}
+}
