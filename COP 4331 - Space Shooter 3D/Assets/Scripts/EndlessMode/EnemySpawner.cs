56a0c5851493736d36d5b85f9e74cff0fbae2809
Kyle Capehart <doctorelyk@gmail.com>
2018-03-14 22:05:42 -0400

    "Endless Mode" Proof of Concept
    When the players pass through one of the triggers it spawns 6 enemies on the next side of the course with a random selection from 18 predetermined spots

---
+++ COP 4331 - Space Shooter 3D/Assets/Scripts/EndlessMode/EnemySpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public static EnemySpawner spawner;
+    private List<Transform> sides = new List<Transform>();
+    public GameObject enemyPrefab;
+    public int index = 0;
+
+
+    void Awake()
+    {
+        if (spawner == null)
+        {
+            spawner = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start ()
+    {
+		foreach(Transform child in transform)
+        {
+            sides.Add(child);
+        }
+	}
+	
+    public void Spawn(int index)
+    {
+        Transform side = sides[index];
+        Transform SpawnPoints = side.gameObject.transform.GetChild(0);
+        List<Transform> spawnPointsList = new List<Transform>();
+
+        foreach(Transform child in SpawnPoints)
+        {
+            spawnPointsList.Add(child);
+        }
+
+        System.Random rng = new System.Random();
+
+        for (int i = 0; i < 6; i++)
+        {
+            Transform position = spawnPointsList[rng.Next(0, 18)];
+            Instantiate(enemyPrefab, position);
+        }
+
+    }
+}
---
+++ COP 4331 - Space Shooter 3D/Assets/Scripts/EndlessMode/SpawnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        int index = EnemySpawner.spawner.index++;
+        EnemySpawner.spawner.Spawn(index);
+    }
+    
+}
